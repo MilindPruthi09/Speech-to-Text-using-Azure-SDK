8c1e9c58d8c0b4b52e66a8820f0b098fef863a60
Milind Pruthi <82656184+MilindPruthi09@users.noreply.github.com>
2022-08-20 20:50:39 +0530

    Add files via upload

---
+++ speaking-clock.py
@@ -0,0 +1,119 @@
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+# Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
+from playsound import playsound
+
+import pyaudio
+import wave
+
+def main():
+    try:
+        global speech_config
+
+        # Get Configuration Settings
+        load_dotenv()
+        cog_key = os.getenv('COG_SERVICE_KEY')
+        cog_region = os.getenv('COG_SERVICE_REGION')
+
+        # Configure speech service
+        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
+        print('Ready to use speech service in:', speech_config.region)
+
+        # Get spoken input
+        command = TranscribeCommand()
+        if command.lower() == 'what time is it?':
+            TellTime()
+
+    except Exception as ex:
+        print(ex)
+
+def TranscribeCommand():
+    command = ''
+
+    # Configure speech recognition
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Speak now...')
+    
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+    audioFile = "output.wav"
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(audioFile, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    audioFile='output.wav'
+    audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+
+    # Process speech input
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
+
+    # Return the command
+    return command
+
+
+def TellTime():
+    now = datetime.now()
+    response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
+
+
+    # Configure speech synthesis
+    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+
+    # Synthesize spoken output
+    speak = speech_synthesizer.speak_text_async(response_text).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
+
+    # Print the response
+    print(response_text)
+
+
+if __name__ == "__main__":
+    main()
